758d3bbffc2769fc1e15a07a26ec2fce62226556
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2025-08-30 14:33:06 +0200

    updated icons

--- MinimalTowerDefense/MinimalTowerDefence.service.worker.js
+++ MinimalTowerDefense/MinimalTowerDefence.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756554825|80775287';
+const CACHE_VERSION = '1756557017|58691021';
 /** @type {string} */
 const CACHE_PREFIX = 'MinimalTowerDefe-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
