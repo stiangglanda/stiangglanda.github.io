b476c332dbb6f5b824d3d553d323ffae36f402e7
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2025-09-29 20:49:05 +0200

    updated mtd to test

--- MinimalTowerDefense/MinimalTowerDefence.service.worker.js
+++ MinimalTowerDefense/MinimalTowerDefence.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759169802|90373764';
+const CACHE_VERSION = '1759171672|304536864';
 /** @type {string} */
 const CACHE_PREFIX = 'MinimalTowerDefe-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
